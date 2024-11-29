4b81f647ce86119527e865289f94f63be3583575
Alejandro Mery <amery@apptly.co>
2024-11-29 23:22:28 +0000

    fresh nuxt structure
    Signed-off-by: Alejandro Mery <amery@apptly.co>

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+
+  srcDir: 'src',
+  serverDir: 'src/server',
+
+  devtools: { enabled: true }
+})
