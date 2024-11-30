aa87786fa2746cc0914a691b63d2a150b4ced862
Alejandro Mery <amery@apptly.co>
2024-11-30 02:20:54 +0000

    feat(eslint): initial eslint integration
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,15 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 // isProduction = false
 const isProduction = process.env.NODE_ENV === 'production';
+// autoFix = true
+const autoFix = !isProduction
+  && process.env.VITE_AUTO_FIX !== 'false'
+  && process.env.VITE_AUTO_FIX !== 'no';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxt/eslint',
     'nitro-cloudflare-dev',
   ],
 
@@ -17,5 +22,17 @@ export default defineNuxtConfig({
 
   nitro: {
     preset: 'cloudflare-pages',
+  },
+
+  eslint: {
+    checker: {
+      lintOnStart: autoFix,
+      fix: autoFix,
+    },
+    config: {
+      stylistic: {
+        semi: true,
+      },
+    },
   },
 });
