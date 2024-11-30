a8ca3938fe158ecb0e2ffffe4790fa35bbfb26c5
Alejandro Mery <amery@apptly.co>
2024-11-30 04:39:24 +0000

    feat(tailwind): enable @poupe/theme-builder with darvaza colours
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,13 @@
 import type { Config } from 'tailwindcss';
+import { withMaterialColors } from '@poupe/theme-builder/tailwind';
 
-export default <Partial<Config>>{};
+// colours from darvaza's logo
+const primary = '#caae88';
+const secondary = '#8ca0a0';
+const tertiary = '#6e7181';
+
+export default withMaterialColors({}, {
+  primary,
+  secondary,
+  tertiary,
+});
