4b21f0d44dccb4d99ded8a1649f31febdcaa69a2
Alejandro Mery <amery@apptly.co>
2024-11-30 00:03:47 +0000

    feat(wrangler): initial cloudflare integration
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,17 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [],
+  modules: [
+    'nitro-cloudflare-dev',
+  ],
 
   devtools: { enabled: !isProduction },
 
   srcDir: 'src',
   serverDir: 'src/server',
   compatibilityDate: '2024-11-01',
+
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
 });
