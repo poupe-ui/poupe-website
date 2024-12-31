904f0308ea4bb4f78b9e4be44f2eb559e6afe8af
Alejandro Mery <amery@apptly.co>
2024-12-31 18:24:01 +0000

    feat(server): initial /api/tailwind placeholder
    Signed-off-by: Alejandro Mery <amery@apptly.co>

---
+++ src/server/api/tailwind/[scheme]/[primary].get.ts
@@ -0,0 +1,67 @@
+import type {
+  H3Event,
+  EventHandlerRequest,
+} from 'h3';
+
+import type {
+  CSSRuleObject,
+} from 'tailwindcss/types/config';
+
+import {
+  rgbFromHct,
+} from '@poupe/theme-builder/tailwind';
+
+import type {
+  Hct,
+  StandardDynamicSchemeKey,
+} from '~/composables/useColor';
+
+interface ThemeOptions {
+  primary: Hct;
+  scheme: StandardDynamicSchemeKey;
+}
+
+function handleThemeRequest(event: H3Event<EventHandlerRequest>, opt: ThemeOptions) {
+  const theme: CSSRuleObject = {
+    '.root': {
+      '--md-primary': rgbFromHct(opt.primary),
+      '--md-scheme': opt.scheme,
+    },
+  };
+
+  setResponseHeaders(event, {
+    'content-type': 'text/plain; charset=utf-8',
+    'cache-control': 'no-cache',
+  });
+
+  return JSON.stringify(theme);
+};
+
+export default defineEventHandler((e) => {
+  const opt: Partial<ThemeOptions> = {
+    primary: themeColorFromRouterParam(e, 'primary'),
+    scheme: themeSchemeFromRouterParam(e, 'scheme'),
+  };
+
+  const errMsg = new Array<string>();
+
+  if (opt.scheme === undefined) {
+    errMsg.push('invalid scheme');
+  }
+
+  if (opt.primary === undefined) {
+    errMsg.push('invalid primary color');
+  }
+
+  if (errMsg.length > 0) {
+    setResponseStatus(e, 400);
+    setResponseHeaders(e, {
+      'content-type': 'text/plain; charset=utf-8',
+      'cache-control': 'no-cache',
+    });
+
+    return errMsg.join('\n');
+  }
+
+  return handleThemeRequest(e, opt as ThemeOptions);
+});
