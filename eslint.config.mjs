a630f9597a608b221963936c2f9e9fed8de380de
Alejandro Mery <amery@apptly.co>
2025-01-04 03:18:33 +0000

    eslint-unicorn [WIP]
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,35 @@
 // @ts-check
+import unicornPlugin from 'eslint-plugin-unicorn';
+
 import { forNuxt } from '@poupe/eslint-config/nuxt';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(...forNuxt());
+export default withNuxt(
+  unicornPlugin.configs['flat/recommended'],
+  ...forNuxt(),
+  {
+    rules: {
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          replacements: {
+            env: {
+              environment: false,
+            },
+            fn: {
+              function: false,
+            },
+            prop: {
+              property: false,
+            },
+            vars: {
+              variables: false,
+            },
+          },
+        },
+      ],
+      'unicorn/no-array-for-each': 'error',
+      '@stylistic/arrow-parens': 'error',
+    },
+  },
+);
