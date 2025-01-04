9884300e2c0f7997aaa73ba9bf32aaea11164a4a
Alejandro Mery <amery@apptly.co>
2025-01-04 03:18:57 +0000

    tailwind.config: only primary
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,7 @@
 import { withMaterialColors } from '@poupe/theme-builder/tailwind';
 
-// colours from darvaza's logo
 const primary = '#caae88';
-const secondary = '#8ca0a0';
-const tertiary = '#6e7181';
 
 export default withMaterialColors({}, {
   primary,
-  secondary,
-  tertiary,
-}, {
-  omitPrintException: true,
 });
