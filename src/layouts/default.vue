25e545fe79e55f2080c96ac5872993c786db1be9
Alejandro Mery <amery@apptly.co>
2025-01-12 13:59:02 +0000

    layout: initial attempt to use css theme from api
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,7 +3,20 @@
 </template>
 
 <script setup lang="ts">
+const primary = '#caae88';
+
 useHead({
+  link: [{
+    id: 'poupe-theme-link',
+    rel: 'stylesheet',
+    href: `/api/tailwindcss/content/${primary.slice(1)}`,
+    fetchPriority: 'high',
+    tagPriority: 0,
+  }],
+  meta: [{
+    name: 'theme-color',
+    content: primary,
+  }],
   bodyAttrs: {
     class: 'bg-surface text-on-surface',
   },
