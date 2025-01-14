4c70c52e60c89c8de3608e293c2b5a94657a7a8f
Alejandro Mery <amery@apptly.co>
2025-01-14 13:57:13 +0000

    feat(components): <toggle-dark />
    Signed-off-by: Alejandro Mery <amery@apptly.co>

---
+++ src/components/toggle-dark.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const sun = 'heroicons:sun-solid';
+const moon = 'heroicons:moon-solid';
+
+const { darkMode } = useTheme();
+</script>
+
+<template>
+  <reka-toggle
+    v-model="darkMode"
+    class="cursor-pointer size-4"
+    as-child
+  >
+    <icon
+      :name="darkMode ? sun : moon"
+    />
+  </reka-toggle>
+</template>
