4234ce7e6a49ed29ad13121331a48fc2bf702c22
Alejandro Mery <amery@apptly.co>
2025-01-19 23:26:00 +0000

    feat(components): fix <toggle-dark /> hydration error
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- src/components/toggle-dark.vue
+++ src/components/toggle-dark.vue
@@ -6,13 +6,21 @@ const { darkMode } = useTheme();
 </script>
 
 <template>
-  <reka-toggle
-    v-model="darkMode"
-    class="cursor-pointer size-4"
-    as-child
-  >
-    <icon
-      :name="darkMode ? sun : moon"
-    />
-  </reka-toggle>
+  <div>
+    <client-only>
+      <template #fallback>
+        <div class="size-4" />
+      </template>
+
+      <reka-toggle
+        v-model="darkMode"
+        class="cursor-pointer size-4"
+        as-child
+      >
+        <icon
+          :name="darkMode ? sun : moon"
+        />
+      </reka-toggle>
+    </client-only>
+  </div>
 </template>
