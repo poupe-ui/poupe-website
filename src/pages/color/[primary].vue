639859852af9a9bc213672f57d6af2f65f554253
Alejandro Mery <amery@apptly.co>
2025-01-24 04:12:14 +0000

    feat(pages): add support for non-hex colors in /color and /theme
    closes #88
    
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- src/pages/color/[primary].vue
+++ src/pages/color/[primary].vue
@@ -11,12 +11,25 @@
 <script setup lang="ts">
 definePageMeta({
   layout: 'themeless',
-
-  validate: (route): boolean => isValidRouteParam('primary', isHexValue, route),
 });
 
-const primary = useHCTColor(useRoute().params.primary) || useRandomColor();
-const themeColor = hexFromHct(primary);
+const $route = useRoute();
+const { param: primaryParam, color: themeColor } = useColorParam($route.params.primary);
+
+if (themeColor === undefined || $route.path.endsWith('/')) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Not Found',
+  });
+}
+
+if (themeColor !== `#${primaryParam}`) {
+  await navigateTo(joinRelativeURL($route.path, `../${themeColor.slice(1)}`), {
+    redirectCode: 308,
+  });
+}
+
+const primary = hct(themeColor);
 const themeURL = computed(() => `/api/tailwindcss/content/${themeColor.slice(1)}`);
 
 useHead({
