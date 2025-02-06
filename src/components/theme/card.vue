048489e8cc35e7081fb0ad465fff2906bb6c913f
Alejandro Mery <amery@apptly.co>
2025-02-06 23:20:35 +0000

    WIP
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- src/components/theme/card.vue
+++ src/components/theme/card.vue
@@ -5,19 +5,24 @@ defineProps<{
 </script>
 
 <template>
-  <div class="surface-bright md:m-2 p-2 md:rounded md:shadow-shadow md:shadow-md">
-    <div class="flex justify-between items-center">
-      <slot name="title">
-        <h1
-          v-if="title"
-          class="text-2xl font-bold"
-        >
-          {{ title }}
-        </h1>
-      </slot>
-      <toggle-dark class="ms-2" />
-    </div>
+  <p-card
+    shadow="lg"
+    surface="bright"
+  >
+    <template #header>
+      <div class="flex justify-between items-center m-2 p-2">
+        <slot name="title">
+          <h1
+            v-if="title"
+            class="text-2xl font-bold"
+          >
+            {{ title }}
+          </h1>
+        </slot>
+        <toggle-dark class="ms-2" />
+      </div>
+    </template>
 
     <slot />
-  </div>
+  </p-card>
 </template>
